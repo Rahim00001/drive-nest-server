a30071a99688ebd508672f5c2b73952c71446b8a
Rahim00001 <rahimlpt2022@gmail.com>
2023-10-20 22:24:55 +0600

    cara data created to database

--- index.js
+++ index.js
@@ -28,104 +28,18 @@ async function run() {
 
         const carCollection = client.db("carDB").collection('car');
 
+        app.get('/car', async (req, res) => {
+            const cursor = carCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
         app.post('/car', async (req, res) => {
             const newCar = req.body;
             console.log(newCar);
             const result = await carCollection.insertOne(newCar);
             res.send(result);
         })
-
-
-
-
-        // const coffeeCollection = client.db("coffeeDB").collection('coffee');
-        // const userCollection = client.db('coffeeDB').collection('user')
-
-
-        // app.get('/coffee', async (req, res) => {
-        //     const cursor = coffeeCollection.find();
-        //     const result = await cursor.toArray();
-        //     res.send(result)
-        // })
-
-        // app.get('/coffee/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await coffeeCollection.findOne(query);
-        //     res.send(result);
-        // })
-
-        // app.post('/coffee', async (req, res) => {
-        //     const newoffee = req.body;
-        //     console.log((newoffee));
-        //     const result = await coffeeCollection.insertOne(newoffee);
-        //     res.send(result)
-        // })
-
-        // app.put('/coffee/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) }
-        //     const options = { upsert: true };
-        //     const updatedCoffee = req.body
-        //     const coffee = {
-        //         $set: {
-        //             name: updatedCoffee.name,
-        //             quantity: updatedCoffee.quantity,
-        //             Supplier: updatedCoffee.Supplier,
-        //             Taste: updatedCoffee.Taste,
-        //             Details: updatedCoffee.Details,
-        //             Category: updatedCoffee.Category,
-        //             Photo: updatedCoffee.Photo
-        //         }
-        //     }
-
-        //     const result = await coffeeCollection.updateOne(filter, coffee, options)
-        //     res.send(result)
-        // })
-
-        // app.delete('/coffee/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await coffeeCollection.deleteOne(query);
-        //     res.send(result);
-        // })
-
-
-        // user related apis
-
-        // app.get('/user', async (req, res) => {
-        //     const cursor = userCollection.find();
-        //     const users = await cursor.toArray();
-        //     res.send(users)
-        // })
-
-
-        // app.post('/user', async (req, res) => {
-        //     const user = req.body;
-        //     console.log((user));
-        //     const result = await userCollection.insertOne(user);
-        //     res.send(result);
-        // });
-
-        // app.patch('/user', async (req, res) => {
-        //     const user = req.body;
-        //     const filter = { email: user.email }
-        //     const updateDoc = {
-        //         $set: {
-        //             lastLoggedAt: user.lastLoggedAt
-        //         }
-        //     }
-        //     const result = await userCollection.updateOne(filter, updateDoc)
-        //     res.send(result);
-        // })
-
-        // app.delete('/user', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await userCollection.deleteOne(query);
-        //     res.send(result);
-        // })
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
